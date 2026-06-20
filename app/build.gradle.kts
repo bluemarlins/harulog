cd56fe9774020e47f85f8b560805f30dc8835b69
Changwoo Kim <spicyrabbit87@gmail.com>
2026-06-20 21:59:52 +0900

    build: disable gradle build abort on lint errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,13 @@ android {
         excludes += "/META-INF/{AL2.0,LGPL2.1}"
       }
     }
+
+    lint {
+        abortOnError = false
+        checkReleaseBuilds = false
+    }
 }
+
 
 kotlin {
     jvmToolchain(17)
